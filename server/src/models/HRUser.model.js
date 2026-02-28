6413f2abfabc2e88bf844c70d49a9744f9f7d555
Durgeshwar-AI <durgesh34shaw94@gmail.com>
2026-02-28 16:39:26 +0530

    changes

--- server/src/models/HRUser.model.js
+++ server/src/models/HRUser.model.js
@@ -5,11 +5,6 @@ const HRUserSchema = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     passwordHash: { type: String, required: true },
-    role: {
-      type: String,
-      enum: ["admin", "viewer"],
-      default: "viewer",
-    },
   },
   { timestamps: true }
 );
